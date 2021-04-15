56ebef3dc9bc821aac2f7d2ef8e6a3c5618dfe93
bellkevin677 <bellkevin677@gmail.com>
2021-04-15 09:25:58 -0500

    completed without bonus

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ function App() {
   return (
     <div>
     <Exercise />
-    <exercise good='Great' />
-    <exercise bad='Not Great' />
+    <Exercise good='Awesome' bad='Terrible'/>
     </div>
   );
 }
--- src/exercise/Exercise.js
+++ src/exercise/Exercise.js
@@ -1,10 +1,19 @@
-const Exercise = ({good = 'Awesome', bad = 'Terrible'}) => {
+import {useState} from 'react'
+
+const Exercise = ({ good = 'Great', bad = 'Not Great' }) => {
+
+    const [num, setNum] = useState(1);
+    const addNum = () => setNum(num + 1);
+
     return (
     <div>
         <h1>{good}</h1>
         <h1>{bad}</h1>
+        <button onClick={(()=> alert('Congrats! You have clicked the button.'))}>CLICK HERE</button>
+        <button onClick={addNum}>STATE</button>
+        <h1>{num}</h1>
     </div>
-    )
+    );
 }
 
 export default Exercise;
